f305383585d061cbbc79835b754831a99039c242
@cheeks cloud <agneskaswii700@gmail.com>
2022-05-10 08:45:49 +0300

    adding the login manager to facilitate a new user login

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
 from app import create_app,db 
 from app.models import User,Role      
 from flask_script import Manager,Server
+from flask_migrate import Migrate, MigrateCommand
 
 
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.command
 def tests():
