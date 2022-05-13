fcc184ddca0ef92178a6274f91cc6f6564cdec05
@cheeks cloud <agneskaswii700@gmail.com>
2022-05-13 12:02:23 +0300

    replacing postgres uri

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ from distutils.debug import DEBUG
 
 
 class Config:
-  
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:cheeks@localhost/piches'
+
   SQLCHEMY_TRACK_MODIFICATIONS = False
   SECRET_KEY ='aNGIE'
   UPLOADED_PHOTOS_DEST = 'app/static/photos'
@@ -20,8 +21,8 @@ class ProdConfig(Config):
 
 class DevConfig(Config):
   
-  SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:cheeks@localhost/piches'
-
+  #SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:cheeks@localhost/piches'
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jjiahbzznlufye:b171e7612ae7f1ebb4f7cd1eb475413d68bf14cd829ef03564c6ac38714859f7@ec2-54-158-247-210.compute-1.amazonaws.com:5432/de2nhnhleqksmo'
   DEBUG=True
 
 
