f0d1de068290f1a8aecd88c40005cd6c0881ed13
@cheeks cloud <agneskaswii700@gmail.com>
2022-05-13 14:21:40 +0300

    creating a new virtual

--- config.py
+++ config.py
@@ -1,29 +1,26 @@
-from distutils.debug import DEBUG
-
-
+import os
 
 class Config:
-  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:cheeks@localhost/piches'
-
-  SQLCHEMY_TRACK_MODIFICATIONS = False
-  SECRET_KEY ='aNGIE'
+  SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:cheeks@localhost/piches' 
+  SQLCHEMY_TRACK_MODIFICATIONS = True
+  SECRET_KEY = os.environ.get('SECRET_KEY')
   UPLOADED_PHOTOS_DEST = 'app/static/photos'
   MAIL_SERVER = 'smtp.googlemail.com'
   MAIL_PORT = 587
   MAIL_USE_TLS = True
-  MAIL_USERNAME = "MAIL_USERNAME"
-  MAIL_PASSWORD = "MAIL_PASSWORD"
+  MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+  MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 class ProdConfig(Config):
-   SQLALCHEMY_DATABASE_URI="DATABASE_URL"
-   if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://",1)
+   SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URL")
+   if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+     SQLALCHEMY_DATABASE_URI=SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
+
 
 class DevConfig(Config):
-  
-  #SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:cheeks@localhost/piches'
-  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jjiahbzznlufye:b171e7612ae7f1ebb4f7cd1eb475413d68bf14cd829ef03564c6ac38714859f7@ec2-54-158-247-210.compute-1.amazonaws.com:5432/de2nhnhleqksmo'
-  DEBUG=True
+   # SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:cheeks@localhost/piches'
+    SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URL")
+    DEBUG=True
 
 
 config_options = {
--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 export SECRET_KEY=aNGIE
 
+
 export MAIL_USERNAME=agneskaswii700@gmail.com
 export MAIL_PASSWORD=Hanselagnes2
+
 python3.8 manage.py server
